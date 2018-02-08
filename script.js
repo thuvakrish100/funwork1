7be8c2d4950a44d41427ffcf1f4ce0998aec110d
thuvakrish100 <thuvakrish100@gmail.com>
2018-02-08 10:58:39 +0530

    change

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     });
   });
 
-  SC.stream('/tracks/345527848', function(sound) {
+  SC.stream('/tracks/173826641', function(sound) {
     $('#start1').click(function(e) {
       e.preventDefault();
       sound.start();
@@ -28,4 +28,106 @@ $(document).ready(function() {
   });
 
 
+  SC.stream('/tracks/335350731', function(sound) {
+    $('#start2').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop2').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
   });
+
+
+
+  SC.stream('/tracks/349414693', function(sound) {
+    $('#start3').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop3').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/354663899', function(sound) {
+    $('#start4').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop4').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/316859203', function(sound) {
+    $('#start5').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop5').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/27107039', function(sound) {
+    $('#start6').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop6').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/317969885', function(sound) {
+    $('#start7').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop7').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/339606057', function(sound) {
+    $('#start8').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop8').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+  SC.stream('/tracks/235717376', function(sound) {
+    $('#start9').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop9').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+});
